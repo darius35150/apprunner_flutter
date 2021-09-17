d690cc4b26fd4f27704918c3b51e38f4bb03069f
Darius Holley <darius35150@yahoo.com>
2021-09-17 10:13:12 -0500

    Added oop concept for opening programs.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:desktop_window/desktop_window.dart';
-// import 'classes/styles/buttonstyles.dart';
+import 'classes/openprograms.dart';
 
 Future<void> main() async {
   runApp(const MyApp());
+
   DesktopWindow.setFullScreen(false);
   await DesktopWindow.setWindowSize(const Size(675, 750));
   await DesktopWindow.setMaxWindowSize(const Size(700, 750));
@@ -39,47 +38,9 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   void _openApplication(String buttonName) {
-    setState(() async {
-      switch (buttonName) {
-        case 'Chrome':
-          Process.run(
-              'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
-              []);
-          break;
-        case 'Firefox':
-          Process.run('C:/Program Files/Mozilla Firefox/firefox.exe', []);
-          break;
-        case 'Eclipse19':
-          Process.run('C:/eclipse.2019/eclipse.exe', []);
-          break;
-        case 'Eclipse20':
-          Process.run('C:/eclipse_20/eclipse.exe', []);
-          break;
-        case 'VS Code':
-          Process.run(
-              'C:/Users/dariush/AppData/Local/Programs/Microsoft VS Code/Code.exe',
-              []);
-          break;
-        case 'Notepad++':
-          Process.run('C:/Program Files (x86)/Notepad++/notepad++.exe', []);
-          break;
-        case 'GitHub':
-          Process.run(
-              'C:/Users/dariush/AppData/Local/GitHubDesktop/GitHubDesktop.exe',
-              []);
-          break;
-        case 'Teams':
-          Process.run(
-              'C:/Users/dariush/AppData/Local/Microsoft/Teams/Update.exe --processStart "Teams.exe"',
-              []);
-          break;
-        case 'Restart':
-          Process.run('shutdown -r -t 0 -f', [], runInShell: true);
-          break;
-        case 'Shutdown':
-          Process.run('shutdown -s -t 0 -f', [], runInShell: true);
-          break;
-      }
+    setState(() {
+      var openApps = OpenPrograms(buttonName);
+      openApps.openApplications();
     });
   }
 
@@ -182,6 +143,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 _openApplication('Shutdown');
               },
               child: const Text("Shutdown"))
-        ]);
+        ]
+    );
   }
 }
